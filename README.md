2dd44c679bb3053ee75ac64e5c976c41ebea8fce
luoxixi128 <eeluox@gmail.com>
2020-10-04 18:56:04 +0800

    version 1.1

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Super Mario music downloader
 
 This downloader can help you automatically download seventeen Mario background music
+
--- mario.py
+++ mario.py
@@ -5,22 +5,20 @@ import os
 with open("mario.txt", "r", encoding='UTF-8') as f:  # open file
     data = f.read()  # read file
     f.close()  # close file
-it = re.finditer(r'http:\\/\\/.+?\.mp3', data)  #
-string = []
+it = re.finditer(r'http:\\/\\/.+?\.mp3', data)  # extract the url by regex
+string = []  # create a array to store the url
 n = 1
-path = "./music"
+path = "./music"  # file path
 if not os.path.exists(path):
-    os.makedirs(path)
+    os.makedirs(path)  # if there isn't such a file then create one
 
 for match in it:
-    string.append(match.group())
-print(string)
+    string.append(match.group())  # store the url to next position
 
 for url in string:
-    clr_format = re.sub(r'\\', "", url)
+    clr_format = re.sub(r'\\', "", url)  # clear the "\"
     print(clr_format)
-    name = "music/" + str(n).zfill(3) + ".mp3"
+    name = "music/" + str(n).zfill(3) + ".mp3"  # name the file
     print(name)
-    urllib.request.urlretrieve(clr_format, name)
-    n += 1   # fuck git
-
+    urllib.request.urlretrieve(clr_format, name)  # access the http sources and name them
+    n += 1
