020e5c5efb8b27fc2b841b615dcf9661c7149eee
luoxixi128 <eeluox@gmail.com>
2020-10-08 12:37:30 +0800

    version 1.1

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 This downloader can help you automatically download seventeen Mario background music
 
+#### version 1.0 :
+
+ You can download the music in the https://voice.baidu.com/Page/soundmuseum?query=%E8%B6%85%E7%BA%A7%E7%8E%9B%E4%B8%BD%E9%9F%B3%E6%95%88&srcid=31354 
+
+#### version 1.1: 
+
+Get the origin code of the page automatically  so that if the website has been updated you can download the new music
+
--- mario.py
+++ mario.py
@@ -1,24 +1,40 @@
 import re
 import urllib.request
 import os
+import requests
+import logging
 
-with open("mario.txt", "r", encoding='UTF-8') as f:  # open file
-    data = f.read()  # read file
-    f.close()  # close file
-it = re.finditer(r'http:\\/\\/.+?\.mp3', data)  # extract the url by regex
-string = []  # create a array to store the url
-n = 1
-path = "./music"  # file path
-if not os.path.exists(path):
-    os.makedirs(path)  # if there isn't such a file then create one
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.DEBUG,
+    datefmt='%Y-%m-%d %H:%M:%S')
+url = "https://voice.baidu.com/Page/soundmuseum"
+queries = {"query": "超级玛丽音效", "srcid": "31354"}
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/68.0.3440.106 Safari/537.36'}
+data = requests.get(url, params=queries, headers=headers)
 
-for match in it:
-    string.append(match.group())  # store the url to next position
 
-for url in string:
-    clr_format = re.sub(r'\\', "", url)  # clear the "\"
-    print(clr_format)
-    name = "music/" + str(n).zfill(3) + ".mp3"  # name the file
-    print(name)
-    urllib.request.urlretrieve(clr_format, name)  # access the http sources and name them
-    n += 1
+def main():
+    it = re.finditer(r'http:\\/\\/.+?\.mp3', data.text)  # extract the url by regex
+    string = []  # create a array to store the url
+    n = 1
+    path = "./music"  # file path
+    if not os.path.exists(path):
+        logging.debug("creating a new directory")
+        os.makedirs(path)  # if there isn't such a file then create one
+
+    for match in it:
+        string.append(match.group())  # store the url to next position
+    logging.debug("Have got all the urls,ready to download")
+    for url in string:
+        logging.debug("Downloading "+str(n).zfill(3) + ".mp3")
+        clr_format = re.sub(r'\\', "", url)  # clear the "\"
+        name = "music/" + str(n).zfill(3) + ".mp3"  # name the file
+        urllib.request.urlretrieve(clr_format, name)  # access the http sources and name them
+        n += 1
+
+
+if __name__ == "__main__":
+    main()
